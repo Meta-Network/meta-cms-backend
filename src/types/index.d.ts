5e5aefbc33d55b01011f5e3317e134b6da2383e8
Garfield550 <openpgp@550.moe>
2021-07-23 07:09:55 +0000

    feat: add types

---
+++ src/types/index.d.ts
@@ -0,0 +1,39 @@
+import { Request } from 'express';
+import * as core from 'express-serve-static-core';
+import { JwtPayload } from 'jsonwebtoken';
+import qs from 'qs';
+
+export interface UCenterUser {
+  bio: string;
+  avatar: string;
+  username: string;
+  nickname: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface UCenterAccount {
+  id: number;
+  user_id: number;
+  account_id: string;
+  platform: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface UCenterJWTPayload extends JwtPayload, UCenterUser {
+  purpose: string;
+  account: UCenterAccount;
+}
+
+export interface RequestBodyWithJWTPayload
+  extends Record<string | number | symbol, any> {
+  jwtPayload: UCenterJWTPayload;
+}
+
+export type RequestWithJWTPayload = Request<
+  core.ParamsDictionary,
+  any,
+  RequestBodyWithJWTPayload,
+  qs.ParsedQs
+>;
