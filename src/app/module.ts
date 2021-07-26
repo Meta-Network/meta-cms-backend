efbf6f46f253f54fdbc5e134c31bf8d8a352f1a3
Garfield550 <openpgp@550.moe>
2021-07-26 10:57:34 +0000

    chore: format code

--- src/app/module.ts
+++ src/app/module.ts
@@ -2,6 +2,7 @@ import { WinstonModule } from 'nest-winston';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JWTAuthGuard } from '../auth/guard';
 import { AuthModule } from '../auth/module';
@@ -12,7 +13,6 @@ import { SiteConfigModule } from '../site/config/module';
 import { SiteInfoModule } from '../site/info/module';
 import { AppController } from './controller';
 import { AppService } from './service';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [
