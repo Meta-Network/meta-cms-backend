477dc4bbc7f04a8156ae829e400581ba108aa017
Garfield550 <openpgp@550.moe>
2021-07-26 11:26:30 +0000

    chore: change typeorm config

---
+++ ormconfig.ts
@@ -0,0 +1,40 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import * as yaml from 'js-yaml';
+import { ConnectionOptions } from 'typeorm';
+
+interface Config {
+  db: {
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    database: string;
+  };
+}
+
+const YAML_CONFIG_FILENAME =
+  process.env.NODE_ENV === 'production'
+    ? 'config.production.yaml'
+    : 'config.development.yaml';
+
+const config: Config = yaml.load(
+  readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
+) as Config;
+
+const options: ConnectionOptions = {
+  type: 'mysql',
+  host: config.db.host,
+  ssl: {
+    ca: readFileSync('./rds-ca-2019-root.pem', 'utf8').toString(),
+  },
+  port: config.db.port || 3306,
+  connectTimeout: 60 * 60 * 1000,
+  username: config.db.username,
+  password: config.db.password,
+  database: config.db.database,
+  entities: ['src/entities/*.ts'],
+  synchronize: false,
+};
+
+export default options;
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,27 +1,17 @@
-import * as fs from 'fs';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 
 @Injectable()
 export class TypeORMConfigService implements TypeOrmOptionsFactory {
-  constructor(private readonly configService: ConfigService) {}
+  // constructor(private readonly configService: ConfigService) {}
 
-  createTypeOrmOptions(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
-    return {
-      type: 'mysql',
-      host: this.configService.get<string>('db.host'),
-      ssl: {
-        ca: fs.readFileSync('./rds-ca-2019-root.pem', 'utf8').toString(),
-      },
-      port: this.configService.get<number>('db.port', 3306),
-      connectTimeout: 60 * 60 * 1000,
-      username: this.configService.get<string>('db.username'),
-      password: this.configService.get<string>('db.password'),
-      database: this.configService.get<string>('db.database'),
+  async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
+    const rootOptions = await getConnectionOptions();
+
+    return Object.assign(rootOptions, {
       autoLoadEntities: true,
-      entities: [],
-      synchronize: false,
-    };
+    });
   }
 }
