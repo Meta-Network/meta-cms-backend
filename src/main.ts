51cb6b23786da4a87c80c5f3bec7099ef4500596
Garfield550 <openpgp@550.moe>
2021-07-27 10:04:25 +0000

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
 import * as cookieParser from 'cookie-parser';
 import * as formCors from 'form-cors';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/module';
 import { RequestNotAcceptableException } from './exceptions';
+import packageJson = require('../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(configService.get<string>('app.name'))
+    .setDescription(packageJson.description)
+    .setVersion(packageJson.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   app.use(cookieParser());
   app.use(
     formCors({
       exception: new RequestNotAcceptableException(),
     }),
   );
-  await app.listen(3000);
+  await app.listen(configService.get<number>('app.port'));
 }
 bootstrap();
