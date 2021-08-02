14adc12b9a91428c6ef82593d0878484a479f790
Garfield550 <openpgp@550.moe>
2021-08-02 14:38:06 +0000

    feat: add utils

---
+++ src/utils/validation.ts
@@ -0,0 +1,29 @@
+import { IsArray, isEmpty, IsIn, validateSync } from 'class-validator';
+import { SiteConfigEntity } from '../entities/siteConfig.entity';
+
+class ResultListClass {
+  constructor(list: boolean[]) {
+    this.resultList = list;
+  }
+  @IsArray()
+  @IsIn([true], { each: true })
+  readonly resultList: boolean[];
+}
+
+export const checkConfigIsDeletable = (config: SiteConfigEntity): boolean => {
+  const checkList = [
+    'storeType',
+    'storeProviderId',
+    'cicdType',
+    'cicdProviderId',
+    'publisherType',
+    'publisherProviderId',
+    'cdnType',
+    'cdnProviderId',
+  ];
+  const resultList = checkList.map((item) => isEmpty(config[item]));
+  const obj = new ResultListClass(resultList);
+  const errors = validateSync(obj);
+  if (errors.length) return false;
+  return true;
+};
