e341d05fbb13fc61c33b3f773e89185d5544040f
Garfield550 <openpgp@550.moe>
2021-08-03 10:34:16 +0000

    pref: type gymnastics

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,8 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { RequestWithUser } from '../types';
+import { RemoveIndex, RequestWithUser, UCenterJWTPayload } from '../types';
 
 export const User = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
+  (data: keyof RemoveIndex<UCenterJWTPayload>, ctx: ExecutionContext) => {
     const req: RequestWithUser = ctx.switchToHttp().getRequest();
     const user = req.user;
     return data ? user[data] : user;
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -20,6 +20,7 @@ export interface UCenterAccount {
 }
 
 export interface UCenterJWTPayload extends JwtPayload, UCenterUser {
+  id: number;
   purpose: 'access_token' | 'refresh_token' | string;
   account: UCenterAccount;
 }
@@ -33,3 +34,11 @@ export interface TransformResponse<T = any> {
   statusCode: number;
   message: string;
 }
+
+export type RemoveIndex<Q> = {
+  [key in keyof Q as string extends key
+    ? never
+    : key extends string
+    ? key
+    : never]: Q[key];
+};
