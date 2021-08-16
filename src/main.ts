341c2237d1713f85d969a426336880f6a9db9516
Garfield550 <openpgp@550.moe>
2021-08-16 16:58:25 +0000

    feat: update winston config

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,48 @@
-import * as cookieParser from 'cookie-parser';
-import formCors from 'form-cors';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import formCors from 'form-cors';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from 'src/api/app/module';
 import { RequestNotAcceptableException } from 'src/exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
+  const appPort = configService.get<number>('app.port', 3000);
+  const enableSwagger = configService.get<boolean>('swagger.enable');
+
+  if (enableSwagger) {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle(configService.get<string>('app.name'))
+      .setDescription('CMS API testing branch')
+      .setVersion(process.env.npm_package_version || '0.0.1')
+      .addCookieAuth('ucenter_access_token')
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document, {
+      customCss: '.swagger-ui tr { display: block; padding: 10px 0; }',
+    });
+  }
+
   app.enableCors();
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle(configService.get<string>('app.name'))
-    .setDescription('CMS API testing branch')
-    .setVersion(process.env.npm_package_version || '0.0.1')
-    .addCookieAuth('ucenter_access_token')
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document, {
-    customCss: '.swagger-ui tr { display: block; padding: 10px 0; }',
-  });
-
   app.use(cookieParser());
   app.use(
     formCors({
       exception: new RequestNotAcceptableException(),
     }),
   );
-  await app.listen(configService.get<number>('app.port'));
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: configService.get<number>('microservice.port', appPort + 1000),
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(appPort);
 }
 bootstrap();
