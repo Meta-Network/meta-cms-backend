27365ed902a6b6f27ab4355ba94c568ea1527eed
Garfield550 <openpgp@550.moe>
2021-08-19 12:32:53 +0000

    feat: add modules

--- src/api/provider/storage/github/module.ts
+++ src/api/provider/storage/github/module.ts
@@ -4,12 +4,16 @@ import { GitHubStorageBaseService } from 'src/api/provider/storage/github/baseSe
 import { GitHubStorageController } from 'src/api/provider/storage/github/controller';
 import { GitHubStorageLogicService } from 'src/api/provider/storage/github/logicService';
 import { SiteConfigModule } from 'src/api/site/config/module';
+import { TasksModule } from 'src/api/task/module';
+import { ThemeTemplateModule } from 'src/api/theme/template/module';
 import { GitHubStorageProviderEntity } from 'src/entities/provider/storage/github.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([GitHubStorageProviderEntity]),
     SiteConfigModule,
+    ThemeTemplateModule,
+    TasksModule,
   ],
   controllers: [GitHubStorageController],
   providers: [GitHubStorageBaseService, GitHubStorageLogicService],
