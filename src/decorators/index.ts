a6d25c809bb168b075bbc487797ca97d846a0f02
Garfield550 <openpgp@550.moe>
2021-08-23 11:00:39 +0000

    feat: update decorators

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -3,17 +3,43 @@ import {
   ExecutionContext,
   SetMetadata,
 } from '@nestjs/common';
+import { Request } from 'express';
 
 import { NestMetadataType } from '../constants';
-import { RemoveIndex, RequestWithUser, UCenterJWTPayload } from '../types';
+import {
+  RemoveIndex,
+  RequestWithUser,
+  UCenterAccount,
+  UCenterJWTPayload,
+} from '../types';
 
-export const User = createParamDecorator(
-  (data: keyof RemoveIndex<UCenterJWTPayload>, ctx: ExecutionContext) => {
-    const req: RequestWithUser = ctx.switchToHttp().getRequest();
-    const user = req.user;
-    return data ? user[data] : user;
-  },
-);
+type UserParamDecoratorReturnType =
+  | UCenterJWTPayload
+  | UCenterAccount
+  | string[]
+  | string
+  | number;
+
+export const User = createParamDecorator<
+  keyof RemoveIndex<UCenterJWTPayload>,
+  ExecutionContext,
+  UserParamDecoratorReturnType
+>((data, ctx) => {
+  const req: RequestWithUser = ctx.switchToHttp().getRequest();
+  const user = req.user;
+  return data ? user[data] : user;
+});
+
+export const BasicAuth = createParamDecorator<
+  unknown,
+  ExecutionContext,
+  string
+>((_, ctx) => {
+  const req: Request = ctx.switchToHttp().getRequest();
+  const header = req.get('Authorization');
+  const secret = header.replace('Basic ', '');
+  return Buffer.from(secret, 'base64').toString();
+});
 
 export const SkipUCenterAuth = (skip: boolean) =>
   SetMetadata(NestMetadataType.SkipUCenterAuth, skip);
