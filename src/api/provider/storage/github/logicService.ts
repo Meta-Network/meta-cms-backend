f3213ae3e8bbaf72a79d86d732f88e376ea24a08
Garfield550 <openpgp@550.moe>
2021-08-26 13:19:44 +0000

    feat: add createGitHubRepoFromConfig in github service

--- src/api/provider/storage/github/logicService.ts
+++ src/api/provider/storage/github/logicService.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { validateOrReject } from 'class-validator';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Octokit } from 'octokit';
 import { DeleteResult } from 'typeorm';
 
 import { GitHubStorageProviderEntity } from '../../../../entities/provider/storage/github.entity';
 import {
+  AccessDeniedException,
   DataAlreadyExistsException,
   DataNotFoundException,
   validationErrorToBadRequestException,
@@ -12,9 +15,23 @@ import { MetaWorker } from '../../../../types/metaWorker';
 import { GitHubStorageBaseService } from '../../../provider/storage/github/baseService';
 import { SiteConfigLogicService } from '../../../site/config/logicService';
 
+type CreateGitHubRepoFromConfig = {
+  status: boolean;
+  size: number;
+  permissions?: {
+    admin: boolean;
+    maintain?: boolean;
+    push: boolean;
+    triage?: boolean;
+    pull: boolean;
+  };
+};
+
 @Injectable()
 export class GitHubStorageLogicService {
   constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService,
     private readonly baseService: GitHubStorageBaseService,
     private readonly configLogicService: SiteConfigLogicService,
   ) {}
@@ -132,5 +149,96 @@ export class GitHubStorageLogicService {
     if (!storage) throw new DataNotFoundException('github storage not found');
 
     return await this.baseService.delete(storage.id);
+  }
+
+  /**
+   * For internal use only
+   */
+  async getStorageConfigById(
+    sid: number,
+  ): Promise<GitHubStorageProviderEntity> {
+    const res = await this.baseService.read(sid);
+    if (!res)
+      throw new DataNotFoundException('GitHub storage config not found');
+    return res;
+  }
+
+  async createGitHubRepoFromConfig(
+    token: string,
+    cfg: GitHubStorageProviderEntity,
+  ): Promise<CreateGitHubRepoFromConfig> {
+    const octokit = new Octokit({ auth: token });
+    const { data: authData } = await octokit.rest.users.getAuthenticated();
+    if (!authData)
+      throw new AccessDeniedException("can not login GitHub with user's token");
+    this.logger.verbose(
+      `Successful login github with ${authData.login}`,
+      GitHubStorageLogicService.name,
+    );
+
+    const { userName, repoName } = cfg;
+
+    try {
+      this.logger.verbose(
+        `Check user GitHub repo permissions`,
+        GitHubStorageLogicService.name,
+      );
+      const { data: repoData } = await octokit.rest.repos.get({
+        owner: userName,
+        repo: repoName,
+      });
+      const { pull, push } = repoData?.permissions;
+      if (!(pull && push)) {
+        this.logger.verbose(
+          `Insufficient GitHub permissions, pull: ${pull}, push: ${push}`,
+          GitHubStorageLogicService.name,
+        );
+        throw new AccessDeniedException(
+          `Insufficient GitHub permissions, pull: ${pull}, push: ${push}`,
+        );
+      }
+      this.logger.verbose(
+        `Repo ${repoData.full_name} already exists, size: ${repoData.size}, pull: ${pull} push: ${push}`,
+        GitHubStorageLogicService.name,
+      );
+      console.log(repoData);
+      return {
+        status: true,
+        size: repoData.size,
+        permissions: repoData.permissions,
+      };
+      // throw new Error('DEBUG'); // TODO: How to check github repo not empty
+    } catch (error) {
+      if (error.status === 404) {
+        this.logger.verbose(
+          `Repo ${userName}/${repoName} does not exists`,
+          GitHubStorageLogicService.name,
+        );
+        if (userName === authData.login) {
+          const { data: repoData } =
+            await octokit.rest.repos.createForAuthenticatedUser({
+              name: repoName,
+              private: true,
+              auto_init: false,
+            });
+          const { pull, push } = repoData?.permissions;
+          this.logger.verbose(
+            `Repo ${repoData.full_name} created, size: ${repoData.size}, pull: ${pull} push: ${push}`,
+            GitHubStorageLogicService.name,
+          );
+          console.log(repoData);
+          return {
+            status: true,
+            size: repoData.size,
+            permissions: repoData.permissions,
+          };
+        }
+        this.logger.verbose(
+          `GitHub user ${authData.login} not match repo owner ${userName}`,
+          GitHubStorageLogicService.name,
+        );
+      }
+      throw error;
+    }
   }
 }
