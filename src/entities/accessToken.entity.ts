40d7afa7311d8a0ff97700649335c2adb640707c
神樹桜乃 <kodama@sakuno.moe>
2021-09-13 18:37:20 +0800

    feat: update access token entity

--- src/entities/accessToken.entity.ts
+++ src/entities/accessToken.entity.ts
@@ -1,18 +1,29 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsInt, IsNotEmpty, IsString } from 'class-validator';
 import { Column, Entity, Index } from 'typeorm';
 
 import { BaseEntity } from './base.entity';
 
 @Entity()
+@Index(['userId', 'platform'], { unique: true })
+@Index(['userId', 'active'])
 export class AccessTokenEntity extends BaseEntity {
+  @Column({ comment: 'UCenter user id' })
+  @IsInt()
+  @IsNotEmpty()
+  userId: number;
+
   @Column({ comment: 'Platform' })
   @IsString()
   @IsNotEmpty()
-  @Index()
   platform: string;
 
   @Column({ comment: 'Access token' })
   @IsString()
   @IsNotEmpty()
   accessToken: string;
+
+  @Column({ type: 'boolean', comment: 'Is actived for synchronizer' })
+  @IsBoolean()
+  @IsNotEmpty()
+  active: boolean;
 }
