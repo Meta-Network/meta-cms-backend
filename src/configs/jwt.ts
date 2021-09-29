1085e2cc7b8a2d088df359ecc81ddb6c4fac14e8
WhyYouAre <whyouare556@protonmail.com>
2021-09-29 15:31:30 +0800

    fix: read public key from sdk - UCenter operators are responsible for updating the key and sdk

--- src/configs/jwt.ts
+++ src/configs/jwt.ts
@@ -2,24 +2,14 @@ import { PUBLIC_KEYS } from '@meta-network/auth-sdk';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
-import { readFileSync } from 'fs';
-import { join } from 'path';
 
-import { CONFIG_PATH } from '../constants';
-
-/**
- * 身份认证用的 JWT_KEY
- */
-export const JWT_KEY = {
-  publicKey: readFileSync(join(CONFIG_PATH, 'JWT_PUBLIC_KEY.pub')),
-};
 @Injectable()
 export class JwtConfigService implements JwtOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
   createJwtOptions(): Promise<JwtModuleOptions> | JwtModuleOptions {
     return {
-      publicKey: JWT_KEY.publicKey,
+      publicKey: PUBLIC_KEYS.DEVELOPMENT,
       verifyOptions: {
         issuer: this.configService.get<string>('jwt.verify.issuer'),
         audience: this.configService.get<string>('jwt.verify.audience'),
